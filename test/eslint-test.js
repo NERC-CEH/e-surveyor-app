1efa99bb33577ed548960675b998bfbda937d946
Karolis Kazlauskis <karolis@flumens.io>
2020-11-25 10:57:04 +0000

    Remove superfluous eslint test runner

--- test/eslint-test.js
+++
@@ -1,8 +0,0 @@
-const lint = require('mocha-eslint');
-
-const paths = ['./+(src|test)/**/*.{js,jsx}'];
-lint(paths, {
-  formatter: 'compact', // Defaults to `stylish`
-  alwaysWarn: false, // Defaults to `true`, always show warnings
-  timeout: 5000, // Defaults to the global mocha `timeout` option
-});
