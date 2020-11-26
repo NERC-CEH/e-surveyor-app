aaef954f5c0f06cc2bb013ce80669d1917d6639c
Karolis Kazlauskis <karolis@flumens.io>
2020-11-26 19:03:10 +0000

    Refactor the tests

--- src/Survey/Report/__test__/index.spec.js
+++ src/Survey/Report/__test__/index.spec.js
@@ -2,6 +2,7 @@ import Sample from 'sample';
 import Occurrence from 'occurrence';
 import Image from 'common/models/image';
 import config from 'Survey/config';
+import seedmixData from 'common/data/seedmix';
 import { getLeagueTable, getMissingSeedmixSpecies } from '..';
 
 describe('Settings Menu', () => {
@@ -34,47 +35,36 @@ describe('Settings Menu', () => {
   });
 
   describe('getMissingSeedmixSpecies', () => {
-    it('should return full missing species list if no species added', () => {
-      // Given
-      const sample = new Sample();
-      sample.attrs.seedmix = 'MIXAB8 -  Flower Rich Margin (AB8)';
-
-      // When
-      const missingSpecies = getMissingSeedmixSpecies(sample);
-
-      // Then
-      expect(missingSpecies.length > 1).toEqual(true);
-    });
-
     it('should return missing species list', () => {
       // Given
-      const seedmixName = 'MIXAB8 -  Flower Rich Margin (AB8)';
-
-      const emptySample = new Sample({ attrs: { seedmix: seedmixName } });
-      const fullSpeciesListSize = getMissingSeedmixSpecies(emptySample).length;
+      const seedmixName = Object.keys(seedmixData)[0];
+      const seedmix = seedmixData[seedmixName];
+      const species = seedmix[0].latin_name;
 
       class SampleWithNoGPS extends Sample {
         startGPS = () => {};
       }
-      const sample = new SampleWithNoGPS();
-      sample.attrs.seedmix = seedmixName;
+      const sample = new SampleWithNoGPS({
+        attrs: {
+          seedmix: seedmixName,
+        },
+      });
 
-      const subSmp = config.smp.create(
-        SampleWithNoGPS,
-        Occurrence,
-        new Image()
-      );
-      subSmp.occurrences[0].media[0].attrs.species = {
-        scientificNameWithoutAuthor: 'Prunella vulgaris',
-      };
-
+      const image = new Image({
+        attrs: {
+          species: {
+            scientificNameWithoutAuthor: species,
+          },
+        },
+      });
+      const subSmp = config.smp.create(SampleWithNoGPS, Occurrence, image);
       sample.samples.push(subSmp);
 
       // When
       const missingSpecies = getMissingSeedmixSpecies(sample);
 
       // Then
-      expect(missingSpecies.length).toEqual(fullSpeciesListSize - 1);
+      expect(missingSpecies.length).toEqual(seedmix.length - 1);
     });
   });
 });
