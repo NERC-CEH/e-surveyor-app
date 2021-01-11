c811c3057582fe283c845358b946bac91ddded48
Karolis Kazlauskis <karolis@flumens.io>
2021-01-11 16:20:32 +0000

    Clean up

--- src/Info/router.jsx
+++ src/Info/router.jsx
@@ -3,16 +3,9 @@ import { Route } from 'react-router-dom';
 import About from './About';
 import Menu from './Menu';
 import Credits from './Credits';
-import MockupSurvey from './MockupSurvey';
 
 export default [
   <Route path="/info/menu" key="/info/menu" exact component={Menu} />,
   <Route path="/info/about" key="/info/about" exact component={About} />,
   <Route path="/info/credits" key="/info/credits" exact component={Credits} />,
-  <Route
-    path="/info/mockup-survey"
-    key="/info/mockup-survey"
-    exact
-    component={MockupSurvey}
-  />,
 ];
