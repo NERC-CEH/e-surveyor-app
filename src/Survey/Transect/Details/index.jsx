49ec0eb3156eb71cfe2c6297c11a5c840cb7720e
Karolis Kazlauskis <karolis@flumens.io>
2021-01-13 19:10:12 +0000

    Add transect details validation

--- src/Survey/Transect/Details/index.jsx
+++ src/Survey/Transect/Details/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Page, Header } from '@apps';
+import { Page, Header, alert } from '@apps';
 import { IonButton, NavContext, IonIcon } from '@ionic/react';
 import { arrowForwardCircleOutline } from 'ionicons/icons';
+import { verifyLocationSchema } from 'Survey/common/config';
+import * as Yup from 'yup';
 import { observer } from 'mobx-react';
 import Main from './Main';
 
@@ -17,13 +19,27 @@ class Controller extends React.Component {
 
   onDone = () => {
     const { match, sample } = this.props;
+      try {
+        Yup.object().shape({
+          location: verifyLocationSchema,
+          seedmix: Yup.mixed().required('Please select your seedmix.'),
+          quadratSize: Yup.number().min(1).required('Please select your seedmix.'),
+          steps: Yup.number().min(1).required('Please select your seedmix.'),
+          habitat: sample.attrs.type === 'Common Standards' && Yup.mixed().required()
+        }).validateSync(sample.attrs)
+      } catch (attrError) {
+        alert({
+          header: 'Missing',
+          message: 'Please fill in all the details in this page before navigating next.',
+          buttons: [{
+            text: 'Got it',
+            role: 'cancel'
+          }]
+        })
+        return ;
+      }
 
-    if (!sample.attrs.type) {
-      // TODO: show warning
-      return;
-    }
-
-    sample.metadata.completedDetails = true;
+      sample.metadata.completedDetails = true;
     sample.save();
 
     const url = match.url.replace('/details', '');
