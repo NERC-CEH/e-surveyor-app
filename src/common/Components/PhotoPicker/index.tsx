18a393447316e77d21ed91a2796fa677a09b6cb8
Karolis Kazlauskis <karolis@flumens.io>
2022-03-07 11:11:31 +0200

    Refactor

--- src/common/Components/PhotoPicker/index.tsx
+++ src/common/Components/PhotoPicker/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { FC, ComponentProps } from 'react';
 import { PhotoPicker } from '@flumens';
 import { useTranslation } from 'react-i18next';
 import { observer } from 'mobx-react';
 import Media from 'models/image';
+import Sample from 'models/sample';
+import Occurrence from 'models/occurrence';
 import config from 'common/config';
 import { getImage, getImageModel } from './imageUtils';
 import './styles.scss';
 
-const AppPhotoPicker = ({ model, ...restProps }) => {
+interface Props extends Omit<ComponentProps<typeof PhotoPicker>, 'getImage'> {
+  model: typeof Sample | typeof Occurrence;
+}
+
+const AppPhotoPicker: FC<Props> = ({ model, ...restProps }) => {
   const { t } = useTranslation();
 
   const promptOptions = {
