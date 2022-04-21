7fd2e5d6d3ea2fdf581081d1f609550258190f6a
Karolis Kazlauskis <karolis@flumens.io>
2022-04-21 09:56:43 +0300

    Refactor species search

--- src/common/Components/TaxonSearch/components/Suggestions/components/Species.tsx
+++ src/common/Components/TaxonSearch/components/Suggestions/components/Species.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { FC } from 'react';
 import { IonItem } from '@ionic/react';
 import './styles.scss';
+import { Species } from '../../../index';
 
-const onClick = (e, species, onSelect) => {
+const onClick = (e: any, species: Species, onSelect: any) => {
   const edit = e.target.tagName === 'BUTTON';
 
   onSelect(species, edit);
@@ -16,10 +16,10 @@ const onClick = (e, species, onSelect) => {
  * @returns {*}
  * @private
  */
-function prettifyName(species, searchPhrase) {
+function prettifyName(species: Species, searchPhrase: string) {
   const foundInName = species.found_in_name;
 
-  const name = species[foundInName];
+  const name = species[foundInName] as string;
 
   const searchPos = name.toLowerCase().indexOf(searchPhrase);
   if (!(searchPos >= 0)) {
@@ -34,11 +34,17 @@ function prettifyName(species, searchPhrase) {
   );
 }
 
-const Species = ({ species, searchPhrase, onSelect }) => {
+type Props = {
+  species: Species & { isRecorded?: boolean };
+  searchPhrase: any;
+  onSelect: (species: Species, edit: boolean) => void;
+};
+
+const SpeciesItem: FC<Props> = ({ species, searchPhrase, onSelect }) => {
   const prettyName = prettifyName(species, searchPhrase);
   const { isRecorded } = species;
 
-  const onClickWrap = e => !isRecorded && onClick(e, species, onSelect);
+  const onClickWrap = (e: any) => !isRecorded && onClick(e, species, onSelect);
   return (
     <IonItem
       className={`search-result ${isRecorded ? 'recorded' : ''}`}
@@ -49,10 +55,4 @@ const Species = ({ species, searchPhrase, onSelect }) => {
   );
 };
 
-Species.propTypes = {
-  species: PropTypes.object.isRequired,
-  searchPhrase: PropTypes.string.isRequired,
-  onSelect: PropTypes.func.isRequired,
-};
-
-export default Species;
+export default SpeciesItem;
---
+++ src/common/Components/TaxonSearch/index.tsx
@@ -0,0 +1,148 @@
+import React, {
+  ForwardRefRenderFunction,
+  useEffect,
+  useState,
+  createRef,
+  forwardRef,
+  RefObject,
+} from 'react';
+import { IonSearchbar, useIonViewDidEnter } from '@ionic/react';
+import SpeciesSearchEngine from './utils/taxon_search_engine';
+import Suggestions from './components/Suggestions';
+import './styles.scss';
+
+export type Species = {
+  /**
+   * Warehouse id
+   */
+  warehouse_id: number;
+  /**
+   * Which name was it found when doing search
+   */
+  found_in_name: 'scientific_name' | 'common_name';
+  /**
+   * Scientific name
+   */
+  scientific_name: string;
+  /**
+   * Common name
+   */
+  common_name?: string;
+  /**
+   * Genus array index
+   */
+  array_id?: number;
+  /**
+   * ?
+   */
+  preferredId?: number;
+  /**
+   * Species array index
+   */
+  species_id?: number;
+  /**
+   * Species name index, to know where found
+   */
+  species_name_id?: number;
+  /**
+   * Species group
+   */
+  group?: number;
+  /**
+   * Common name synonym
+   */
+  synonym?: string;
+};
+
+const MIN_SEARCH_LENGTH = 2;
+
+const annotateRecordedTaxa = (
+  searchResults: Species[],
+  recordedTaxa: number[]
+) => {
+  const recordedTaxaWrap = (result: Species) =>
+    recordedTaxa.includes(result.preferredId || result.warehouse_id)
+      ? { ...result, ...{ isRecorded: true } }
+      : result;
+
+  return searchResults.map(recordedTaxaWrap);
+};
+
+type Props = {
+  onSpeciesSelected: any;
+  recordedTaxa?: number[];
+};
+
+const TaxonSearch: ForwardRefRenderFunction<any, Props> = (
+  { recordedTaxa, onSpeciesSelected },
+  ref
+) => {
+  const localRef = createRef<any>();
+  const input = (ref as RefObject<any>) || localRef;
+
+  const [searchPhrase, setSearchPhrase] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<any>(null);
+
+  useEffect(() => {
+    SpeciesSearchEngine.init();
+  }, []);
+
+  const onInputKeystroke = async (e: any) => {
+    let newSearchPhrase = e.target.value;
+
+    const isValidSearch =
+      typeof newSearchPhrase === 'string' &&
+      newSearchPhrase.length >= MIN_SEARCH_LENGTH;
+    if (!isValidSearch) {
+      setSearchPhrase('');
+      setSearchResults(null);
+      return;
+    }
+
+    newSearchPhrase = newSearchPhrase.toLowerCase();
+
+    // search
+    const newSearchResults = await SpeciesSearchEngine.search(newSearchPhrase);
+
+    const annotatedSearchResults = annotateRecordedTaxa(
+      newSearchResults,
+      recordedTaxa || []
+    );
+
+    setSearchPhrase(newSearchPhrase);
+    setSearchResults(annotatedSearchResults);
+  };
+
+  const onInputClear = () => {
+    setSearchPhrase('');
+    setSearchResults(null);
+  };
+
+  useIonViewDidEnter(() => {
+    if (input.current) {
+      input.current.setFocus();
+    }
+  }, [input]);
+
+  return (
+    <>
+      <IonSearchbar
+        id="taxon"
+        ref={input}
+        placeholder="Species name"
+        debounce={300}
+        onIonChange={onInputKeystroke}
+        onIonClear={onInputClear}
+        showCancelButton="never"
+      />
+
+      <Suggestions
+        searchResults={searchResults}
+        searchPhrase={searchPhrase}
+        onSpeciesSelected={onSpeciesSelected}
+      />
+    </>
+  );
+};
+
+export default forwardRef(TaxonSearch);
