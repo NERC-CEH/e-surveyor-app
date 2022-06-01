8ab347a860c55463c350dc1e58e9783640ccb993
Karolis Kazlauskis <karolis@flumens.io>
2022-06-01 12:30:22 +0300

    Remove logout data discard toggle

--- src/Home/Menu/index.tsx
+++ src/Home/Menu/index.tsx
@@ -1,10 +1,8 @@
 import { observer } from 'mobx-react';
-import { IonItem, IonLabel, IonCheckbox } from '@ionic/react';
 import { Page, useAlert, useToast, useLoader } from '@flumens';
 import { Trans as T } from 'react-i18next';
 import appModel from 'models/app';
 import userModel from 'models/user';
-import savedSamples from 'models/savedSamples';
 import Main from './Main';
 import './styles.scss';
 
@@ -12,26 +10,16 @@ const useConfirmationDialog = () => {
   const alert = useAlert();
 
   return (callback: any) => {
-    let deleteData = false;
-
-    const onCheckboxChange = (e: any) => {
-      deleteData = e.detail.checked;
-    };
-
     alert({
       header: 'Logout',
       message: (
-        <>
-          <T>Are you sure you want to logout?</T>
+        <T>
+          Are you sure you want to logout?
           <br />
           <br />
-          <IonItem lines="none" className="log-out-checkbox">
-            <IonLabel>
-              <T>Discard local data</T>
-            </IonLabel>
-            <IonCheckbox onIonChange={onCheckboxChange} />
-          </IonItem>
-        </>
+          Your pending and uploaded <b>surveys will not be deleted </b> from
+          this device.
+        </T>
       ),
       buttons: [
         {
@@ -42,7 +30,7 @@ const useConfirmationDialog = () => {
         {
           text: 'Logout',
           cssClass: 'primary',
-          handler: () => callback(deleteData),
+          handler: () => callback(),
         },
       ],
     });
@@ -56,14 +44,7 @@ const Controller = () => {
 
   function logOut() {
     console.log('Info:Menu: logging out.');
-    const resetWrap = async (reset: boolean) => {
-      if (reset) {
-        appModel.attrs['draftId:point'] = '';
-        appModel.attrs['draftId:transect'] = '';
-        await savedSamples.resetDefaults();
-      }
-
-      appModel.attrs.transects = [];
+    const resetWrap = async () => {
       userModel.logOut();
     };
     showLogoutConfirmationDialog(resetWrap);
