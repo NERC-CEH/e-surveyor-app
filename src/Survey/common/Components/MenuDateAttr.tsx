ef9fd15b1bcbb1e2973efb5c99ca4379a7d5ae95
Karolis Kazlauskis <karolis@flumens.io>
2026-05-04 17:46:42 +0300

    Allow empty date values in the date picker

--- src/Survey/common/Components/MenuDateAttr.tsx
+++ src/Survey/common/Components/MenuDateAttr.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { calendarOutline } from 'ionicons/icons';
 import {
   IonDatetime,
@@ -8,29 +9,35 @@ import {
   IonModal,
 } from '@ionic/react';
 
-type Props = { record: any; isDisabled?: boolean };
+type Props = { value?: any; onChange: any; isDisabled?: boolean };
 
-const MenuDateAttr = ({ record, isDisabled }: Props) => (
+const MenuDateAttr = ({ value, onChange, isDisabled }: Props) => (
   <IonItem className="m-0! rounded-none! [--border-radius:0]! [--border-style:solid]! [--inner-padding-end:8px]!">
     <IonIcon src={calendarOutline} slot="start" />
     <IonLabel className="!opacity-100">Date</IonLabel>
     <div className="flex items-center gap-1">
       <div>
-        <IonDatetimeButton
-          datetime="surveyEndTime"
-          slot="end"
-          className="[--ion-text-color:var(--form-value-color)]"
-        />
+        <div className={clsx(!value && 'bg-(--form-value-color)/5 rounded-md')}>
+          <IonDatetimeButton
+            datetime="surveyEndTime"
+            slot="end"
+            className={clsx(
+              '[--ion-text-color:var(--form-value-color)]',
+              !value && '[&::part(native)]:opacity-0'
+            )}
+            onClick={() => {
+              if (!value)
+                onChange({ detail: { value: new Date().toISOString() } });
+            }}
+          />
+        </div>
         <IonModal keepContentsMounted className="[--border-radius:10px]">
           <IonDatetime
             id="surveyEndTime"
             presentation="date"
             preferWheel
-            onIonChange={(e: any) => {
-              // eslint-disable-next-line
-              record.date = e.detail.value.split('T')[0];
-            }}
-            value={record.date}
+            onIonChange={onChange}
+            value={null}
             disabled={isDisabled}
             max={new Date().toISOString()}
           />
