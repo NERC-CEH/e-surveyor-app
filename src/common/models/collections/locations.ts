f3b5cf0f8344a6bc6156b61b1885dc5e0ec7d189
Karolis Kazlauskis <karolis@flumens.io>
2026-07-24 11:54:11 +0100

    Add location media remove pull in

--- src/common/models/collections/locations.ts
+++ src/common/models/collections/locations.ts
@@ -74,8 +74,13 @@ export class LocationsCollection<
     this.remote.synchronising = true;
 
     const docs = await this.fetchRemoteByType(LocType.Site);
+    const mediaDocs = await this.fetchRemoteMedia();
+
     const Model = this.Model as unknown as typeof Location;
-    const newModels = docs.map(doc => Model.fromDTO(doc) as T);
+    const newModels = docs.map(doc => {
+      const media = mediaDocs.filter(m => m.parentId === doc.cid);
+      return Model.fromDTO(doc, { media }) as T;
+    });
     this.upsert(...newModels);
     await Promise.all(newModels.map(m => m.save()));
 
@@ -124,6 +129,59 @@ export class LocationsCollection<
       const docs = res.data.map(getValues).map(unverboseify);
 
       docs.forEach(dtoSchema.parse);
+
+      return docs;
+    } catch (error: any) {
+      if (axios.isCancel(error)) return [];
+
+      if (isAxiosNetworkError(error))
+        throw new HandledError(
+          'Request aborted because of a network issue (timeout or similar).'
+        );
+
+      if ('issues' in error) {
+        const err: ZodError = error;
+        throw new Error(
+          err.issues.map(e => `${e.path.join(' ')} ${e.message}`).join(' ')
+        );
+      }
+
+      throw error;
+    }
+  }
+
+  private async fetchRemoteMedia(): Promise<any[]> {
+    const url = `${this.remote.url}/index.php/services/rest/location-media`;
+
+    const token = await userModel.getAccessToken();
+
+    const options = {
+      params: {},
+      headers: { Authorization: `Bearer ${token}` },
+      timeout: 80000,
+    };
+
+    try {
+      const res = await axios.get(url, options);
+
+      const getValues = (doc: any) =>
+        mapKeys(doc.values, (_, key) =>
+          key.includes(':') ? key : camelCase(key)
+        );
+
+      const docs = res.data.map(getValues).map((doc: any) => ({
+        id: doc.id,
+        cid: doc.id,
+        createdAt: new Date(doc.createdOn).getTime(),
+        updatedAt: new Date(doc.updatedOn).getTime(),
+        syncedAt: Date.now(),
+        data: {
+          type: 'jpeg',
+          data: `${this.remote.url}/upload/${doc.path}`,
+        },
+        parentId: doc.locationId,
+        metadata: {},
+      }));
 
       return docs;
     } catch (error: any) {
